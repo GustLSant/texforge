babd711ae26f6b699b9ad4237c60296348aa5cf2
Gustavo LS <gustlsantdev@gmail.com>
2025-03-10 15:49:33 -0300

    rework zoomTextureCanvas and DraggableTexture

--- src/components/DraggableTexture/DraggableTexture.tsx
+++ src/components/DraggableTexture/DraggableTexture.tsx
@@ -1,27 +1,32 @@
 import React from "react";
 
+type PropsDraggableTexture = {
+    zoom:number
+}
+
 
 let offsetX = 0;
 let offsetY = 0;
 let isDragging = false;
 
 
-export default function DraggableTexture(){
+export default function DraggableTexture({zoom}:PropsDraggableTexture){
     const divRef = React.useRef<HTMLDivElement | null>(null);
 
 
     function handleMouseDown(e:React.MouseEvent<HTMLDivElement>){
         if(!divRef.current) return;
         isDragging = true;
-        offsetX = e.clientX - divRef.current.offsetLeft;
-        offsetY = e.clientY - divRef.current.offsetTop;
+        offsetX = (e.clientX)/zoom - divRef.current.offsetLeft;
+        offsetY = (e.clientY)/zoom - divRef.current.offsetTop;
+
         document.body.style.userSelect = "none"; // desativa seleção de texto enquanto arrasta o mouse
     };
 
-    function handleMouseMove(e:MouseEvent){
+    function handleMouseMove(e:React.MouseEvent<HTMLDivElement>){
         if (!isDragging || !divRef.current) return;
-        divRef.current.style.left = `${e.clientX - offsetX}px`;
-        divRef.current.style.top = `${e.clientY - offsetY}px`;
+        divRef.current.style.left = `${(e.clientX)/zoom - offsetX}px`;
+        divRef.current.style.top = `${(e.clientY)/zoom - offsetY}px`;
     };
 
     function handleMouseUp(){
@@ -30,14 +35,8 @@ export default function DraggableTexture(){
     }
 
 
-    React.useEffect(()=>{
-        document.addEventListener("mousemove", handleMouseMove);
-        document.addEventListener("mouseup", handleMouseUp);
-    }, [])
-
-
     return(
-        <div ref={divRef} style={{left: "100px", top: "100px"}} className="draggable-texture w-20 h-20 bg-red-500 absolute hover:cursor-pointer" onMouseDown={handleMouseDown}>
+        <div ref={divRef} style={{left: "100px", top: "100px", zoom: zoom}} className="draggable-texture w-20 h-20 bg-red-500 absolute hover:cursor-move" onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
 
         </div>
     )
